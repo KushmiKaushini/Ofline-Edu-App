4da3897619aee14757ca9b178dfdd2692f3cb978
Maleesha Maduwantha <163408015+KushmiKaushini@users.noreply.github.com>
2025-02-28 22:43:16 +0530

    Finished Code

--- app/src/main/java/com/example/oflineeduapp/content1Activity.java
+++ app/src/main/java/com/example/oflineeduapp/content1Activity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -27,7 +26,7 @@ public class content1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_content1);
+        setContentView(R.layout.activity_student_con);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
